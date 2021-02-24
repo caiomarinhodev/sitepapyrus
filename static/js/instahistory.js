13094c3ad40d907253a65fe47a1efa5685ebe685
Caio Marinho <caio.barbosa@virtus.ufcg.edu.br>
2021-02-24 19:17:55 -0300

    add protocol

--- static/js/instahistory.js
+++ static/js/instahistory.js
@@ -51,14 +51,14 @@
             // Determine what type of search we are going to make.
             if (settings.get.indexOf('#') > -1) {
                 searchType = 'tag';
-                searchUrl = 'http://www.instagram.com/explore/tags/' + settings.get.split('#')[1] + '/?__a=1';
+                searchUrl = location.protocol+'//www.instagram.com/explore/tags/' + settings.get.split('#')[1] + '/?__a=1';
                 if (settings.limit > 68) {
                     settings.limit = 68;
                     console.log("The max number of images for a tag is 68");
                 }
             } else if (settings.get.indexOf('@') > -1) {
                 searchType = 'user';
-                searchUrl = 'http://www.instagram.com/' + settings.get.split('@')[1] + '/?__a=1';
+                searchUrl = location.protocol+'//www.instagram.com/' + settings.get.split('@')[1] + '/?__a=1';
                 if (settings.limit > 12) {
                     settings.limit = 12;
                     console.log("The max number of images for a user is 24");
@@ -83,7 +83,7 @@
                                 class: 'img-responsive img-thumbnail'
                             });
                             imageUrl = '';
-                            linkUrl = 'http://www.instagram.com/p/' + post.shortcode + '/';
+                            linkUrl = location.protocol+'//www.instagram.com/p/' + post.shortcode + '/';
 
                             if (settings.squareImages) {
                                 var size; // is an index in an array
